108f8a21a081b919d0ee14070cac647a52e1c069
Eugene Obukhov <evgeniy.d.obuhov@bia-tech.ru>
2016-09-12 10:16:18 +0300

    Correct response

--- 08_templating/tutorial/views.py
+++ 08_templating/tutorial/views.py
@@ -5,10 +5,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='home.pt')
 def home(request):
     return {'name': 'Home view'}
-    return Response('<body>Visit <a href="/howdy">hello</a></body>')
 
 
 @view_config(route_name='hello', renderer='home.pt')
 def hello(request):
     return {'name': 'Hello view'}
-    return Response('<body>Go back <a href="/">home</a></body>')
